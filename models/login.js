c03288c8fe5b113e3ef6090a42949477463e6ca1
Arunkumar Palaniappan <11667975+arunkumarpalaniappan@users.noreply.github.com>
2018-03-29 12:05:28 +0530

    payload

--- models/login.js
+++ models/login.js
@@ -1,9 +1,9 @@
 'use strict';
 const Promise = require("bluebird");
 const isValidUser = require("../lib/isValidUser").isValidUser;
-exports.authenticateUser = function (payload) {
+exports.authenticateUser = function () {
     return new Promise((resolve,reject) => {
-        if (isValidUser(payload)) {
+        if (isValidUser(this)) {
             resolve({status: true})
         } else {
             reject(false)
